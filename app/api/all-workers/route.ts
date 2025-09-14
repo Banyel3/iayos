528fde53eefdc29a545f5683092be2b82209b97c
Banyel <189988764+OnlyBanyel@users.noreply.github.com>
2025-09-15 01:46:52 +0800

    Update type definitions and API route handler
    Removed unused 'req' parameter from GET handler in all-workers API route. Updated type definitions in generated Prisma files to use 'BigInt' and 'Object' for improved type accuracy.

--- app/api/all-workers/route.ts
+++ app/api/all-workers/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
-export async function GET(req: Request) {
+export async function GET() {
   try {
     const Jobresults = await prisma.worker_Profile.findMany({
       select: {
