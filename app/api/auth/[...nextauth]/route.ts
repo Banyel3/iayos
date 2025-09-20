2ab5405df110dbb491b6f6a7361a4fa071889300
Banyel <189988764+OnlyBanyel@users.noreply.github.com>
2025-09-20 22:34:57 +0800

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -80,7 +80,7 @@ export const authOptions: NextAuthOptions = {
       credentials: {
         email: {},
         password: {},
-        turnstilToken: {},
+        turnstileToken: {},
       },
       /**
        * CREDENTIAL VALIDATION FUNCTION
@@ -116,20 +116,20 @@ export const authOptions: NextAuthOptions = {
           );
         }
 
-        const verify = await fetch(
-          "https://challenges.cloudflare.com/turnstile/v0/siteverify",
-          {
-            method: "POST",
-            headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: `secret=${process.env.TURNSTILE_SECRET_KEY}&response=${token}`,
-          }
-        ).then((res) => res.json());
+        // const verify = await fetch(
+        //   "https://challenges.cloudflare.com/turnstile/v0/siteverify",
+        //   {
+        //     method: "POST",
+        //     headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        //     body: `secret=${process.env.TURNSTILE_SECRET_KEY}&response=${token}`,
+        //   }
+        // ).then((res) => res.json());
 
-        if (!verify.success) {
-          return new Response(JSON.stringify({ error: "Captcha failed" }), {
-            status: 400,
-          });
-        }
+        // if (!verify.success) {
+        //   return new Response(JSON.stringify({ error: "Captcha failed" }), {
+        //     status: 400,
+        //   });
+        // }
         // Look up user in database by email, include their profile data
         const user = await prisma.accounts.findUnique({
           where: { email: credentials?.email },
