e76a3c8cd43d37a0e378f1d6e2c4e5742eb587c3
Banyel <138186128+Banyel3@users.noreply.github.com>
2025-11-14 16:00:18 +0800

    Bug Fix

---
+++ apps/frontend_mobile/iayos_mobile/components/index.ts
@@ -0,0 +1,5 @@
+// Component barrel exports for Phase 5
+export { AvatarUpload } from "./AvatarUpload";
+export { PortfolioUpload } from "./PortfolioUpload";
+export { PortfolioGrid } from "./PortfolioGrid";
+export { ImageViewer } from "./ImageViewer";
